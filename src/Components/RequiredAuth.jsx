3aa30b2947a17023b2714ae00760e6341e675a4a
Rojalini Das <rojalini08das@gmail.com>
2022-10-31 19:46:43 +0530

    my-shop completed

---
+++ src/Components/RequiredAuth.jsx
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "./AuthContext";
+
+export const RequiredAuth = ({ children }) => {
+  const { token } = useContext(AuthContext);
+  return token ? (
+    children
+  ) : (
+    <div className="h-72 w-full flex items-center justify-center">
+      <h3 className="mt-20 text-center text-4xl">
+        Please
+        <Link to="/login" className="text-indigo-600 mx-4">
+          login
+        </Link>
+        to access this page
+      </h3>
+    </div>
+  );
+};
